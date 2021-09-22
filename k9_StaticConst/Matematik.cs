a70e456402a5379c06c202efd60c5cb4664529e2
batuhanuvem <batuhanuvem@gmail.com>
2021-09-22 13:49:00 +0300

    dalabum

--- k9_StaticConst/Matematik.cs
+++ k9_StaticConst/Matematik.cs
@@ -8,6 +8,16 @@ namespace k9_StaticConst
 {
     class Matematik
     {
+        public const double EulerSabiti = 0.577721; // Bu değer programın her noktasında bu sayı ile çalışacak, sonradan değeri değiştirilemez.
 
+        public const double PisagorSabiti = 1.4142195; //Karekök 2'nin değeri.
+
+        public static double KarelerFakiniAl(double sayi1, double sayi2)
+        {
+            var sayi1Karesi = Math.Pow(sayi1, 2);
+            var sayi2Karesi = Math.Pow(sayi2, 2);
+            var karelerFarki = sayi1Karesi - sayi2Karesi;
+            return karelerFarki;
+        }
     }
 }
